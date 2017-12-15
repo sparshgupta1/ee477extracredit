98da8eefe66238c7159800e4fbf3538443d515a9
Sparsh Gupta <sparshgupta@Sparshs-MacBook-Pro.local>
2017-12-14 18:08:03 -0800

    all event handling done

--- index.js
+++ index.js
@@ -3,3 +3,104 @@ function change1a () {
   $('#2d').addClass('selected');
   $('#2e').addClass('selected');
 }
+function change1b () {
+  $('#2a').addClass('selected');
+  $('#2b').addClass('selected');
+  $('#2c').addClass('selected');
+  $('#2d').addClass('selected');
+  $('#2e').addClass('selected');
+  $('#2f').addClass('selected');
+}
+function change1c () {
+  $('#2a').addClass('selected');
+  $('#2d').addClass('selected');
+  $('#2e').addClass('selected');
+  $('#2f').addClass('selected');
+  $('#2g').addClass('selected');
+}
+function change1d () {
+  $('#2g').addClass('selected');
+  $('#2h').addClass('selected');
+  $('#2i').addClass('selected');
+  $('#2j').addClass('selected');
+  $('#2k').addClass('selected');
+}
+function change1e () {
+  $('#2a').addClass('selected');
+  $('#2b').addClass('selected');
+  $('#2c').addClass('selected');
+  $('#2d').addClass('selected');
+  $('#2k').addClass('selected');
+  $('#2l').addClass('selected');
+}
+
+function change2a () {
+  $('#3a').addClass('selected');
+  $('#3c').addClass('selected');
+}
+function change2b () {
+  $('#3a').addClass('selected');
+  $('#3b').addClass('selected');
+  $('#3d').addClass('selected');
+  $('#3e').addClass('selected');
+  $('#3f').addClass('selected');
+  $('#3g').addClass('selected');
+}
+function change2c () {
+  $('#3a').addClass('selected');
+  $('#3b').addClass('selected');
+  $('#3d').addClass('selected');
+  $('#3e').addClass('selected');
+  $('#3f').addClass('selected');
+  $('#3g').addClass('selected');
+}
+function change2d () {
+  $('#3a').addClass('selected');
+  $('#3b').addClass('selected');
+  $('#3d').addClass('selected');
+  $('#3e').addClass('selected');
+  $('#3f').addClass('selected');
+  $('#3g').addClass('selected');
+  $('#3h').addClass('selected');
+}
+function change2e () {
+  $('#3a').addClass('selected');
+  $('#3c').addClass('selected');
+  $('#3d').addClass('selected');
+}
+function change2f () {
+  $('#3a').addClass('selected');
+  $('#3d').addClass('selected');
+  $('#3e').addClass('selected');
+  $('#3f').addClass('selected');
+}
+function change2g () {
+  $('#3a').addClass('selected');
+  $('#3c').addClass('selected');
+}
+function change2h () {
+  $('#3a').addClass('selected');
+  $('#3c').addClass('selected');
+  $('#3g').addClass('selected');
+}
+function change2i () {
+  $('#3a').addClass('selected');
+  $('#3c').addClass('selected');
+  $('#3d').addClass('selected');
+  $('#3e').addClass('selected');
+}
+function change2j () {
+  $('#3a').addClass('selected');
+  $('#3c').addClass('selected');
+}
+function change2k () {
+  $('#3d').addClass('selected');
+  $('#3e').addClass('selected');
+  $('#3f').addClass('selected');
+  $('#3g').addClass('selected');
+  $('#3h').addClass('selected');
+}
+function change2l () {
+  $('#3d').addClass('selected');
+  $('#3e').addClass('selected');
+}
