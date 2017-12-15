21d0957aac5d8fb4e67978e502395002b1fa3be0
Sparsh Gupta <sparshgupta@Sparshs-MacBook-Pro.local>
2017-12-14 23:16:43 -0800

    change animation to highlight only

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-
-function change1a () {
-  $('#2a').removeClass('selected').width();
-  $('#2d').removeClass('selected').width();
-  $('#2e').removeClass('selected').width();
-  $('#2a').addClass('selected');
-  $('#2d').addClass('selected');
-  $('#2e').addClass('selected');
-}
-function change1b () {
+function notSelected2Level(){
   $('#2a').removeClass('selected').width();
   $('#2b').removeClass('selected').width();
   $('#2c').removeClass('selected').width();
   $('#2d').removeClass('selected').width();
   $('#2e').removeClass('selected').width();
   $('#2f').removeClass('selected').width();
+  $('#2g').removeClass('selected').width();
+  $('#2h').removeClass('selected').width();
+  $('#2i').removeClass('selected').width();
+  $('#2j').removeClass('selected').width();
+  $('#2k').removeClass('selected').width();
+  $('#2l').removeClass('selected').width();
+}
+function notSelected3Level(){
+  $('#3a').removeClass('selected').width();
+  $('#3b').removeClass('selected').width();
+  $('#3c').removeClass('selected').width();
+  $('#3d').removeClass('selected').width();
+  $('#3e').removeClass('selected').width();
+  $('#3f').removeClass('selected').width();
+  $('#3g').removeClass('selected').width();
+  $('#3h').removeClass('selected').width();
+}
+function change1a () {
+  notSelected2Level();
+  notSelected3Level();
+  $('#2a').addClass('selected');
+  $('#2d').addClass('selected');
+  $('#2e').addClass('selected');
+}
+function change1b () {
+  notSelected2Level();
+  notSelected3Level();
   $('#2a').addClass('selected');
   $('#2b').addClass('selected');
   $('#2c').addClass('selected');
@@ -22,11 +40,8 @@ function change1b () {
   $('#2f').addClass('selected');
 }
 function change1c () {
-  $('#2a').removeClass('selected').width();
-  $('#2d').removeClass('selected').width();
-  $('#2e').removeClass('selected').width();
-  $('#2f').removeClass('selected').width();
-  $('#2g').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#2a').addClass('selected');
   $('#2d').addClass('selected');
   $('#2e').addClass('selected');
@@ -34,11 +49,8 @@ function change1c () {
   $('#2g').addClass('selected');
 }
 function change1d () {
-  $('#2g').removeClass('selected').width();
-  $('#2h').removeClass('selected').width();
-  $('#2i').removeClass('selected').width();
-  $('#2j').removeClass('selected').width();
-  $('#2k').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#2g').addClass('selected');
   $('#2h').addClass('selected');
   $('#2i').addClass('selected');
@@ -46,12 +58,8 @@ function change1d () {
   $('#2k').addClass('selected');
 }
 function change1e () {
-  $('#2a').removeClass('selected').width();
-  $('#2b').removeClass('selected').width();
-  $('#2c').removeClass('selected').width();
-  $('#2d').removeClass('selected').width();
-  $('#2k').removeClass('selected').width();
-  $('#2l').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#2a').addClass('selected');
   $('#2b').addClass('selected');
   $('#2c').addClass('selected');
@@ -61,18 +69,14 @@ function change1e () {
 }
 
 function change2a () {
-  $('#3a').removeClass('selected').width();
-  $('#3c').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3c').addClass('selected');
 }
 function change2b () {
-  $('#3a').removeClass('selected').width();
-  $('#3b').removeClass('selected').width();
-  $('#3d').removeClass('selected').width();
-  $('#3e').removeClass('selected').width();
-  $('#3f').removeClass('selected').width();
-  $('#3g').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3b').addClass('selected');
   $('#3d').addClass('selected');
@@ -81,12 +85,8 @@ function change2b () {
   $('#3g').addClass('selected');
 }
 function change2c () {
-  $('#3a').removeClass('selected').width();
-  $('#3b').removeClass('selected').width();
-  $('#3d').removeClass('selected').width();
-  $('#3e').removeClass('selected').width();
-  $('#3f').removeClass('selected').width();
-  $('#3g').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3b').addClass('selected');
   $('#3d').addClass('selected');
@@ -95,13 +95,8 @@ function change2c () {
   $('#3g').addClass('selected');
 }
 function change2d () {
-  $('#3a').removeClass('selected').width();
-  $('#3b').removeClass('selected').width();
-  $('#3d').removeClass('selected').width();
-  $('#3e').removeClass('selected').width();
-  $('#3f').removeClass('selected').width();
-  $('#3g').removeClass('selected').width();
-  $('#3h').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3b').addClass('selected');
   $('#3d').addClass('selected');
@@ -111,59 +106,50 @@ function change2d () {
   $('#3h').addClass('selected');
 }
 function change2e () {
-  $('#3a').removeClass('selected').width();
-  $('#3c').removeClass('selected').width();
-  $('#3d').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3c').addClass('selected');
   $('#3d').addClass('selected');
 }
 function change2f () {
-  $('#3a').removeClass('selected').width();
-  $('#3d').removeClass('selected').width();
-  $('#3e').removeClass('selected').width();
-  $('#3f').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3d').addClass('selected');
   $('#3e').addClass('selected');
   $('#3f').addClass('selected');
 }
 function change2g () {
-  $('#3a').removeClass('selected').width();
-  $('#3c').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3c').addClass('selected');
 }
 function change2h () {
-  $('#3a').removeClass('selected').width();
-  $('#3c').removeClass('selected').width();
-  $('#3g').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3c').addClass('selected');
   $('#3g').addClass('selected');
 }
 function change2i () {
-  $('#3a').removeClass('selected').width();
-  $('#3c').removeClass('selected').width();
-  $('#3d').removeClass('selected').width();
-  $('#3e').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3c').addClass('selected');
   $('#3d').addClass('selected');
   $('#3e').addClass('selected');
 }
 function change2j () {
-  $('#3a').removeClass('selected').width();
-  $('#3c').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3a').addClass('selected');
   $('#3c').addClass('selected');
 }
 function change2k () {
-  $('#3d').removeClass('selected').width();
-  $('#3e').removeClass('selected').width();
-  $('#3f').removeClass('selected').width();
-  $('#3g').removeClass('selected').width();
-  $('#3h').removeClass('selected').width();
+  notSelected2Level();
+  notSelected3Level();
   $('#3d').addClass('selected');
   $('#3e').addClass('selected');
   $('#3f').addClass('selected');
@@ -171,9 +157,8 @@ function change2k () {
   $('#3h').addClass('selected');
 }
 function change2l () {
-  $('#3d').removeClass('selected').width();
-  $('#3e').removeClass('selected').width();
-
+  notSelected2Level();
+  notSelected3Level();
   $('#3d').addClass('selected');
   $('#3e').addClass('selected');
 }
